a15486866b7850550bd98adc83f0dcf9a1153470
RGero215 <rgero215@gmail.com>
2017-01-17 14:07:05 -0400

    first commit

---
+++ MemeMe App/MemeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  MemeMe App
+//
+//  Created by Ramon Geronimo on 1/9/17.
+//  Copyright © 2017 Ramon Geronimo. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ MemeMe App/MemeTableViewController .swift
@@ -0,0 +1,9 @@
+//
+//  MemeTableViewController .swift
+//  MemeMe App
+//
+//  Created by Ramon Geronimo on 1/9/17.
+//  Copyright © 2017 Ramon Geronimo. All rights reserved.
+//
+
+import Foundation
